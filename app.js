10e991fa8c32a657eadef2bff5cb54307b5b8a46
Matt Jiles <mjiles1@gmail.com>
2017-07-17 23:49:56 -0400

    setting up sequelize

--- app.js
+++ app.js
@@ -22,7 +22,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// controllers
 app.use(require('./controllers'));
+
+// models
+const models = require("./models");
+
+//Sync Database
+models.sequelize.sync().then(function(){
+console.log('Nice! Database looks fine')
+
+}).catch(function(err){
+console.log(err,"Something went wrong with the Database Update!")
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/users/register.js
+++ controllers/users/register.js
@@ -2,8 +2,16 @@ const express = require('express');
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('register');
+router.get('/', (req, res, next) => {
+    res.render('users/register');
+});
+
+router.post('/', (req, res, next) => {
+    console.log(req.body.username);
+    
+    const db = require('../../db.js');
+
+    res.redirect('/');
 });
 
 module.exports = router;
